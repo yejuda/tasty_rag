855dbf07579a40aa6ed7734f6328765147148b3e
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-01-04 18:00:59 +0900

    FEAT: 레스토랑 데이터 검색

--- data/search_place.py
+++ data/search_place.py
@@ -1,17 +1,29 @@
+from typing import Dict, List
 import requests
 
-def get_restarnts_info(city: str, page_index: int):
+from assets.api_len import api_length_dict
+
+
+def search_restaurant(place: str, city: str):
+    i = 1
+    data = get_restaurants_info(city, i)
+    for res in data:
+        print(res['BIZPLC_NM'])
+    # while i <= api_length_dict[city]:
+    #     data = get_restaurants_info(city, i)
+    #     for res in data:
+
+def get_restaurants_info(city: str, page_index: int) -> List:
     BASE_URL = "https://openapi.gg.go.kr/GENRESTRT?Type=json"
     response = requests.get(f"{BASE_URL}&SIGUN_NM={city}&pIndex={page_index}")
 
     if response.status_code != 200:
-        return None
-
-    return response.json()
+        return []
+    return response.json()['GENRESTRT'][1]["row"]
 
 if __name__ == "__main__":
-    data = get_restarnts_info("구리시", 1)
-    import pprint
+    data = search_restaurant("잉꼬칼국수", "구리시")
+    # import pprint
 
-    if data:
-        pprint.pprint(data)
+    # if data:
+    #     pprint.pprint(data)
