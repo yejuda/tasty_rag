52f46ab7daa6b25837c2ab98301a34a8f136997d
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-01-09 19:20:26 +0900

    FEAT: DB 구축 후 리뷰 저장하도록 작성

---
+++ data/database/db_crud.py
@@ -0,0 +1,58 @@
+from typing import Dict, List
+
+from sqlite_base import SessionLocal, init_db
+from models import Restaurant, Review
+
+
+init_db()
+db_session = SessionLocal()
+
+
+def search_restaurant(name: str) -> bool:
+    if db_session.query(Restaurant.name == name).first():
+        return True
+    return False
+
+
+def add_restaurant(name: str, address: str, reviews: List[str]):
+    if not search_restaurant(name):
+        res = Restaurant(name=name, address=address)
+        db_session.add(res)
+        db_session.commit()
+        
+        res_id = res.id
+
+        for review in reviews:
+            db_session.add(Review(restaurant_id=res_id, context=review))
+        db_session.commit()
+
+
+# def remove_restaurant(name: str):
+#     target = db_session.query(Restaurant).filter(Restaurant.name == name).first()
+#     if target:
+#         db_session.delete(target)
+#         db_session.commit()
+
+def get_restaurant_db(name: str) -> Dict[str, List[str]]:
+    target = db_session.query(Restaurant).filter(Restaurant.name == name).first()
+    if target:
+        review_arr = []
+        for review in target.reviews:
+            review_arr.append(review.context)
+        return {"name": target.name, "address": target.address, "reviews": review_arr}
+    return {}
+
+# def show_restaurant() -> Dict[str, List[str]]:
+#     lists = db_session.query(Restaurant).all()
+#     result = {}
+#     for li in lists:
+#         result[li.name] = [li.address, li.review]
+#     return result
+
+
+if __name__ == "__main__":
+    # add_restaurant("의현", "남양주시", ["맛있어요"])
+    print(get_restaurant_db("의현"))
+    # print(show_restaurant())
+    # remove_restaurant("의현")
+    # print(show_restaurant())
